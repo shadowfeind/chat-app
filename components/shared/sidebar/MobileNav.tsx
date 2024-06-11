725374d348b306a0219b5150c932d379940f4ad8
shadowfeind <travestroy007@gmail.com>
2024-06-11 10:45:18 +0545

    mobile and deskto navigation finished

---
+++ components/shared/sidebar/MobileNav.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useNavigation } from "@/hooks/useNavigation";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const MobileNav = (props: Props) => {
+  const paths = useNavigation();
+  return (
+    <Card className="fixed bottom-4 w-[calc(100vw-32px)] flex items-center h-16 p-2 lg:hidden">
+      <nav className="w-full">
+        <ul className="flex items-center justify-evenly">
+          {paths.map((path, id) => (
+            <li key={id} className="relative">
+              <Link href={path.href}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Button
+                      size="icon"
+                      variant={path.active ? "default" : "outline"}
+                    >
+                      {path.icon}
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{path.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </Link>
+            </li>
+          ))}
+          <li>
+            <div>
+              <UserButton />
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </Card>
+  );
+};
+
+export default MobileNav;
--- components/shared/sidebar/SidebarWrapper.tsx
+++ components/shared/sidebar/SidebarWrapper.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
 
 type Props = {
   children: React.ReactNode;
@@ -7,6 +9,8 @@ type Props = {
 const SidebarWrapper = ({ children }: Props) => {
   return (
     <div className="h-full w-full p-4 flex flex-col lg:flex-row gap-4">
+      <DesktopNav />
+      <MobileNav />
       <main className="h-[calc(100%-80px)] lg:h-full w-full flex gap-4">
         {children}
       </main>
